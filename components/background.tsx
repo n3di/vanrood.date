ec16af1de475e88f946ecb39453cd2af5f39cf28
Michał Szwindowski <github@nedi.me>
2023-04-23 20:04:38 +0200

    v1.0.2

--- components/background.tsx
+++ components/background.tsx
@@ -14,7 +14,7 @@ function Background() {
           loop={true}
           effect={"fade"}
           autoplay={{
-            delay: 8000,
+            delay: 16000,
             disableOnInteraction: false,
           }}
           modules={[EffectFade, Autoplay]}
@@ -25,8 +25,6 @@ function Background() {
               <Image
                 src="https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/a129287628ef45a668f0_vanrood_44220253979_3816192307//thumb/thumb967640754-1920x1080.jpg"
                 alt="Picture from vanRooD Stream"
-                layout="fill"
-                objectFit="cover"
               />
             </div>
           </SwiperSlide>
@@ -35,8 +33,6 @@ function Background() {
               <Image
                 src="https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/4f493b013a6f8560fd66_vanrood_74543487350_3265019413//thumb/thumb946326704-1920x1080.jpg"
                 alt="Picture from vanRooD Stream"
-                layout="fill"
-                objectFit="cover"
               />
             </div>
           </SwiperSlide>
@@ -45,8 +41,6 @@ function Background() {
               <Image
                 src="https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/6223dae6dd7625776f94_vanrood_48267204149_2697502352//thumb/thumb941791913-1920x1080.jpg"
                 alt="Picture from vanRooD Stream"
-                layout="fill"
-                objectFit="cover"
               />
             </div>
           </SwiperSlide>
@@ -55,8 +49,6 @@ function Background() {
               <Image
                 src="https://static-cdn.jtvnw.net/cf_vods/d1m7jfoe9zdc1j/231f7c33e51ecf199e97_vanrood_35817211879_1963204099//thumb/thumb980606013-1920x1080.jpg"
                 alt="Picture from vanRooD Stream"
-                layout="fill"
-                objectFit="cover"
               />
             </div>
           </SwiperSlide>
