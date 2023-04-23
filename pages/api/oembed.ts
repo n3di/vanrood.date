4300ead4e340f2c5c254e5b081caf2debb695775
Michał Szwindowski <github@nedi.me>
2023-04-23 19:21:51 +0200

    v1.0.0

--- pages/api/oembed.ts
+++ pages/api/oembed.ts
@@ -2,9 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function oEmbed(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).send({
-    author_name: "Michał Szwindowski",
-    author_url: "http://www.nedi.me",
-    provider_name: "Nedi",
+    provider_name: "nedi.me",
     provider_url: "http://www.nedi.me",
   });
 }
